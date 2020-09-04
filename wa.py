57dacf44d2c7eeea7b940c6279bba93940e1973b
Churritzu <israel@obejanegra.com.mx>
2020-09-04 14:46:48 -0700

    Added conf file and func to read the file

--- wa.py
+++ wa.py
@@ -1,4 +1,11 @@
 #! /usr/bin/python3
 
+import json
+
+def read_conf_file():
+  with open("./wa-conf.json") as conf_file:
+      data = json.load(conf_file)
+      print(data)
+
 if __name__ == "__main__":
-    pass
+    read_conf_file()

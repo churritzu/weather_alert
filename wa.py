e237ff892148410010608f4a028d68a2ef352fc8
Churritzu <churritzu@yahoo.com>
2021-07-10 17:29:00 -0700

    Imprement de log definition

--- wa.py
+++ wa.py
@@ -20,9 +20,9 @@ messages = {
 			"503": "Servicio no disponible."
 		},
 		"response":{
-			"alert": "Como quieres tus huevos al sol?",
-			"warning": "Se necesita prender la refrigeracion",
-			"normal": "A todo dar, ten un buen dia",
+			"alert": "Como quieres tus huevos al sol? 🥵",
+			"warning": "Se necesita prender la refrigeracion 😥",
+			"normal": "A todo dar, ten un buen dia 🥶",
 		}
 	},
 	"en":{
@@ -38,6 +38,8 @@ messages = {
 	}
 }
 
+lang_strings = None
+
 def log(message, type = ErrorTypes.INFO):
 	diafecha = str(datetime.datetime.now())
 	full_message = type + " [" + diafecha + "]: " + message
@@ -51,40 +53,45 @@ def read_conf_file():
   return data
 
 def get_params(data):
-  data = read_conf_file()
-  if data['api'] in data : return data[data['api']]
-  else: 
-    print("[ERROR]: The api your looking not exist.")
-    return None
+	if data['api'] in data : return data[data['api']]
+	else: 
+		print(log(lang_strings['errors']['404'], ErrorTypes.ALERT))
+		return None
 
 def get_open_weather_data(params):
-  city = "q="+params['city']
-  state = ","+params['state']
-  country = ","+params['country']
-  appKey = "&appid="+params['api_key']
-  lang = "&lang=es"
-  unit = "&units=metric"
-  req = requests.get(params['base_url']+'?'+city+state+country+lang+unit+appKey)
-  if req.status_code == 200: return req.json()
-  else: 
-    print("[ERROR] Server not found")
-    return None
+	city = "q="+params['city']
+	state = ","+params['state']
+	country = ","+params['country']
+	appKey = "&appid="+params['api_key']
+	lang = "&lang=es"
+	unit = "&units=metric"
+	req = requests.get(params['base_url']+'?'+city+state+country+lang+unit+appKey)
+	if req.status_code == 200: return req.json()
+	else: 
+		print(log(lang_strings['errors']['503'], ErrorTypes.ALERT))
+		return None
 
 if __name__ == "__main__":
-  config = read_conf_file()
-  params = get_params(config)
+	config = read_conf_file()
+	lang_strings = messages[config['lang']]
 
-  while True:
-    data = get_open_weather_data(params)
-    temp = data['main']['temp']
-    print(temp)
-    if temp >= 50:
-      print("Como quieres tus huevos al sol???")
-    elif temp < 50 and temp >= 40:
-      print("Se necesita prender la refrigeracion :(")
-    else:
-      print("A todo dar, ten un buen dia :)")
+	params = get_params(config)
 
-    time.sleep(config['sleep']*60)
+	while True:
+		data = get_open_weather_data(params)
+		temp = data['main']['temp']
+		tempString = "Temperatura: " + str(temp) + " C"
+		print(tempString)
+		if temp >= 50:
+			log(tempString, ErrorTypes.ALERT)
+			print(lang_strings['response']['alert'])
+		elif temp < 50 and temp >= 40:
+			log(tempString, ErrorTypes.WARNING)
+			print(lang_strings['response']['warning'])
+		else:
+			log(tempString)
+			print(lang_strings['response']['normal'])
+
+		time.sleep(config['sleep']*60)
 
 
